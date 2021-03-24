33fe223091550b76bebcf2dfc8abcd98bf61cb87
Joao Ceia <joao.p.ceia@gmail.com>
2021-03-24 03:29:05 +0000

    changed main.c

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jpceia <jpceia@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/10 01:38:59 by jpceia            #+#    #+#             */
-/*   Updated: 2021/03/24 03:01:39 by jpceia           ###   ########.fr       */
+/*   Updated: 2021/03/24 03:28:17 by jpceia           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,15 @@ int main(int argc, char **argv)
 
 	int fd = open(argv[1], O_RDONLY);
 	char *str;
+	int e;
 
-	while (get_next_line(fd, &str))
+	e = 1;
+	while (e)
 	{
+		e = get_next_line(fd, &str);
 		printf("%s\n", str);
+		if (e <= 0)
+			break;
 	}
 
 	close(fd);
